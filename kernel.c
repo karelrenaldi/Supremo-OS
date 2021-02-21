a0c70e030e16fc53508c49c9bdd35b2de052a328
supremokr <karelrenaldi8@gmail.com>
2021-02-22 02:22:40 +0700

    finish drawing box on graphical mode

--- kernel.c
+++ kernel.c
@@ -2,14 +2,18 @@ void handleInterrupt21 (int AX, int BX, int CX, int DX);
 void printString(char *string);
 void readString(char *string);
 void clear(char *buffer, int length);
+void drawing();
 
 int main () {
-  char *string_output = "ORANG";
+  char *string_output = "Hello, World !";
   char *string_input = "";
   makeInterrupt21();
-  handleInterrupt21(0x0, string_output, 0, 0);
-  handleInterrupt21(0x1, string_input, 0, 0);
-  handleInterrupt21(0x0, string_input, 0, 0);
+  interrupt(0x10, 0x13, 0x0, 0x0, 0x0);
+  // handleInterrupt21(0x0, string_output, 0x0, 0x0);
+  drawing();
+  handleInterrupt21(0x1, string_input, 0x0, 0x0);
+  interrupt(0x10, 0x03, 0x0, 0x0, 0x0);
+  // handleInterrupt21(0x0, string_input, 0x0, 0x0);
 
   while (1);
 }
@@ -39,14 +43,26 @@ void printString(char *string) {
 
 void readString(char* string) {
   int i = 0;
-  char in = 0x0;
-  while(in != 0x0d) {
-    in = interrupt(0x16, 0x0, 0x0, 0x0, 0x0);
-    *(string + i) = in;
-    interrupt(0x10, (0x0e << 8) + in, 0x0, 0x0, 0x0);
+  char input = 0x0;
+  while(input != 0x0d) {
+    input = interrupt(0x16, 0x0, 0x0, 0x0, 0x0);
+    string[i] = input;
+    interrupt(0x10, (0x0e << 8) + input, 0x0, 0x0, 0x0);
 
-    if (in == 13) interrupt(0x10, (0x0e << 8) + 10, 0x0, 0x0, 0x0);
+    if (input == 0x0d) 
+      interrupt(0x10, (0x0e << 8) + 10, 0x0, 0x0, 0x0);
     i++;
   }
-  *(string + i) = 0x0;
+  string[i] = 0x0;
+}
+
+void drawing() {
+  int i, j;
+  int x_length = 50;
+  for(i = x_length; i != 0; i--) {
+    int y_length = 50;
+    for(j = y_length; j != 0; j--) {
+      interrupt (0x10, (0x0c << 8) + 0x0c, 0x0, i + 0xc, j + 0xc);
+    }
+  }
 }
