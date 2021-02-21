c408f583cd4b598d90a583c789750ea33c39c466
wundersmith <13519192@std.stei.itb.ac.id>
2021-02-21 22:08:07 +0700

    added readstring

--- kernel.c
+++ kernel.c
@@ -4,9 +4,12 @@ void readString(char *string);
 void clear(char *buffer, int length);
 
 int main () {
-  char *string = "Karel";
+  char *string_output = "ORANG";
+  char *string_input = "";
   makeInterrupt21();
-  handleInterrupt21(0, string, 0, 0);
+  handleInterrupt21(0x0, string_output, 0, 0);
+  handleInterrupt21(0x1, string_input, 0, 0);
+  handleInterrupt21(0x0, string_input, 0, 0);
 
   while (1);
 }
@@ -25,9 +28,25 @@ void handleInterrupt21 (int AX, int BX, int CX, int DX) {
 }
 
 void printString(char *string) {
-  int i;
-  while(*(string + i) != '\0'){
-    interrupt (0x10, (0x0e << 8) + *(string + i), 0, 0, 0);
+  int i = 0;
+  while(string[i] != 0){
+    interrupt (0x10, (0x0e << 8) + *(string + i), 0x0, 0x0, 0x0);
     i++;
   }
+  interrupt (0x10, (0x0e << 8) + '\r', 0x0, 0x0, 0x0);
+  interrupt (0x10, (0x0e << 8) + '\n', 0x0, 0x0, 0x0);
+}
+
+void readString(char* string) {
+  int i = 0;
+  char in = 0x0;
+  while(in != 0x0d) {
+    in = interrupt(0x16, 0x0, 0x0, 0x0, 0x0);
+    *(string + i) = in;
+    interrupt(0x10, (0x0e << 8) + in, 0x0, 0x0, 0x0);
+
+    if (in == 13) interrupt(0x10, (0x0e << 8) + 10, 0x0, 0x0, 0x0);
+    i++;
+  }
+  *(string + i) = 0x0;
 }
