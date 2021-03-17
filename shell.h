ea2aa56d7d42b289fa2edc6f48577c289159ebf0
supremokr <karelrenaldi8@gmail.com>
2021-03-17 16:29:32 +0700

    finish shell system

--- shell.h
+++ shell.h
@@ -1,9 +1,12 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+void ln(char* pathReference, char* filename, char currIdx);
 void runShell();
 void cwd(char parentIndex, char* buffer);
 void ls(char currentIndex);
 void cat(char* pathReference, char currIdx);
+int checkCommand(char* input, char* availableCommand);
+void cd(char* path);
 
 #endif
